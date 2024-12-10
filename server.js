ee3e048082d4cc8774d6927c8fce79332583660c
HRC10 <hugo.castor15@gmail.com>
2024-12-10 18:23:28 -0300

    add and configuration cors

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import express from "express";
 import PublicRouter from "./routes/public.js"
 import PrivateRouter from "./routes/private.js"
 import auth from "./middleware/auth.js";
+import cors from "cors"
+
+
 const server = express();
 server.use(express.json());
+server.use(cors())
+
 
 server.use("/", PublicRouter)
 server.use("/", auth, PrivateRouter)
